045065ba5de8f2b9bfc8cc4a03e61e729c9df556
patricksantos01 <patricksantos890@gmail.com>
2024-06-14 21:17:27 -0300

    improving the program output

--- main.py
+++ main.py
@@ -23,14 +23,19 @@ while True:
             file = open('todos.txt', 'r')
             todos = file.readlines()
             file.close()
-            
-            for index, item in enumerate(todos):
+
+            new_todos = [item.strip('\n') for item in todos ]
+
+            for index, item in enumerate(new_todos):
                 print(f"{index +1} -{item}")
+
         case 'edit':
+
             todo_number = int(input("Number of the todo to edit: "))
             todo_number = todo_number -1
             new_todo = input("Enter new todo: ")
             todos[todo_number] = new_todo
+
         case 'complete':
             todo_number = int(input("Number of the todo to complete: "))
             todos.pop(todo_number -1)
