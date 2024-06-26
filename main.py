27e948678131cb26908fa541a8d7896acd28a3d6
patricksantos01 <patricksantos890@gmail.com>
2024-06-26 20:09:24 -0300

    improving the code

--- main.py
+++ main.py
@@ -2,20 +2,17 @@
 while True:
     user_action = input("Type add, show, complete, edit or exit: ")
     user_action = user_action.strip()
+  
+    if 'add' in user_action:
+        # adding a \n to break a line in todos.txt
+        todo = input("Enter a todo: ") + "\n"
 
-    match user_action:
-        case 'add':
-            # adding a \n to break a line in todos.txt
-            todo = input("Enter a todo: ") + "\n"
+        with open('todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+        todos.append(todo)
 
-            todos.append(todo)
-
-            
-
-        case 'show':
+    elif 'show' in user_action:
 
             with open('todos.txt', 'r') as file:
                 todos = file.readlines()
@@ -24,7 +21,7 @@ while True:
                 item = item.strip('\n')
                 print(f"{index +1} -{item}")
 
-        case 'edit':
+    elif 'edit' in user_action:
 
             todo_number = int(input("Number of the todo to edit: "))
             todo_number = todo_number -1
@@ -35,9 +32,7 @@ while True:
             new_todo = input("Enter new todo: ")
             todos[todo_number] = new_todo + '\n'
 
-            
-
-        case 'complete':
+    elif 'complete' in user_action:
             todo_number = int(input("Number of the todo to complete: "))
 
             with open('todos.txt', 'r') as file:
@@ -53,6 +48,5 @@ while True:
             message = f"Todo {todo_to_remove} was removed from the list"
             print(message)
 
-
-        case 'exit':
+    elif 'exit' in user_action:
             break
